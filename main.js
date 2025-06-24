e3c164a4733db64d26b4dcc9b7f25553f827eec6
Nely <nelymurtazina@gmail.com>
2025-06-24 17:16:00 +0300

    update convert valute

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-function getValue() {
-  const input = document.getElementById('numberInput');
-  const number = parseFloat(input.value)
-  if (isNaN(number)) {
-    document.getElementById('result').textContent = 'Пожалуйста, введите число!';
-    return;
+const ValuteConst = {
+  dollar: 78.40,
+  evro: 90.14, 
+  uan: 10.91,
+  lira:1.98,
+};
+
+const allValute = document.getElementById('allValute');
+const VvodInput = document.getElementById('GetInput');
+const GetButton = document.getElementById('GetButton');
+const resultElement = document.getElementById('result');
+
+GetButton.addEventListener('click', () => {
+  const valueSelect = allValute.value;
+  const parseValue = parseFloat(VvodInput.value);
+
+  if(!isNaN(parseValue) && parseValue > 0) {
+    const konst = ValuteConst[valueSelect];
+    const convertValute = parseValue * konst;
+
+    resultElement.textContent = `Результат: ${parseValue} ${valueSelect.toUpperCase()} = ${convertValute.toFixed(2)} RUB`;
+  } else {
+    resultElement.textContent = 'Ошибка: Неверная валюта!'
   }
-
-  const dollar = '78.40';
-  const resuolt = (number * dollar).toFixed(2);
-
-  document.getElementById('result').textContent = `Результат: ${resuolt}`;
-}
-
-function getValueEvro() {
-  const input = document.getElementById('numberInputEvro');
-  const number = parseFloat(input.value);
-  if (isNaN(number)) {
-    document.getElementById('result').textContent = 'Пожалуйста, введите число!';
-    return;
-  }
-  const evro = 90.14;
-  const resultEvro = (number * evro).toFixed(2);
-  document.getElementById('evro').textContent = `${resultEvro}`
-}
+})
